58026aa66d0823a15e3cd540fabbb5c33643214e
DadCoach <pavtonomov@gmail.com>
2023-02-05 17:16:37 +0300

    Разбор первой программы. [#159033 #353030]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Pavel Avtonomov");
         System.out.println("12.05.1996");
     }
