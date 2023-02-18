cd542ed9597f6a360f3f685522e4aeb8cf4e34a3
DadCoach <pavtonomov@gmail.com>
2023-02-18 21:07:10 +0300

    6. Идеальный вес [#41584 #354361]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,37 +1,22 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
 
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        Calculator.plus(1, 2);
-        Calculator.plus(10, 11);
+        plus(1, 2);
+        plus(10, 11);
+        minus(7, 3);
+        minus(2, 6);
     }
 
-    public static class ArgMethod {
-        public static void hello(String name, int age) {
-            System.out.println("Hello, " + name + ", age = " + age);
-        }
-
-        public static void main(String[] args) {
-            String name = "Job4j";
-            int age = 6;
-            ArgMethod.hello(name, age);
-            ArgMethod.hello(name, age);
-            ArgMethod.hello(name, age);
-            ArgMethod.hello(name, age);
-        }
-    }
-
-    public static class Echo {
-    }
-
-    public static class Info {
-        public static void main(String[] args) {
-            System.out.println("05.02.2023");
-        }
-    }
 }
