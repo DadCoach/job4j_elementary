8a531a08dd75e8f6d7b17131f6825e98803bbfa7
DadCoach <pavtonomov@gmail.com>
2023-02-18 20:38:49 +0300

    калькулятор

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -10,4 +10,28 @@ public class Calculator {
         Calculator.plus(1, 2);
         Calculator.plus(10, 11);
     }
+
+    public static class ArgMethod {
+        public static void hello(String name, int age) {
+            System.out.println("Hello, " + name + ", age = " + age);
+        }
+
+        public static void main(String[] args) {
+            String name = "Job4j";
+            int age = 6;
+            ArgMethod.hello(name, age);
+            ArgMethod.hello(name, age);
+            ArgMethod.hello(name, age);
+            ArgMethod.hello(name, age);
+        }
+    }
+
+    public static class Echo {
+    }
+
+    public static class Info {
+        public static void main(String[] args) {
+            System.out.println("05.02.2023");
+        }
+    }
 }
