150dbaf5becb4614a1719ccdd4711b9e7947e957
DadCoach <pavtonomov@gmail.com>
2023-04-16 11:21:31 +0300

    4.6.2.

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -1,16 +1,10 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(double p, double k) {
-        double h = p / (2 * (k + 1));
-        double l = h * k;
-        return l * h;
-    }
-
-    public static void main(String[] args) {
-        double result1 = square(10,23);
-        double result2 = square(6, 2);
-        System.out.println(" p = 10, k = 23, s = 1, real = " + result1);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result2);
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 }
